7fc28f310c2b8ca9ff36ca5e70f27b14811a1a7c
AJAY-10001 <127173787+Ajay-10001@users.noreply.github.com>
2023-10-06 20:20:52 +0530

    Add files via upload

---
+++ Practical 2.c
@@ -0,0 +1,14 @@
+//Let's Start Ajay
+#include <stdio.h>
+main()
+{
+    int no; 
+    printf("PRACTICAL 2 BY AJAY\nWAP to find whether a given no is even or odd");
+    printf("\nENTER THE NUMBER TO TEST : ") ;
+    scanf("%d", &no);
+    if ( no % 2 == 0) 
+        printf("GIVEN NUMBER IS EVEN");
+    else
+        printf("GIVEN NUMBER IS ODD");  
+    return 0 ;     
+}    
