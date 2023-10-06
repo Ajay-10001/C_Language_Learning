cb17fe31b21705fc5b911bc44d57bc4aaf7c3e0e
AJAY-10001 <127173787+Ajay-10001@users.noreply.github.com>
2023-10-06 20:26:23 +0530

    Add files via upload

---
+++ Practical 3.c
@@ -0,0 +1,14 @@
+//Let's Start Ajay
+#include <stdio.h>
+main()
+{
+    float b, h, ar; 
+    printf("PRACTICAL 3 BY AJAY\nWAP to calculate the area of triangle");
+    printf("\nENTER THE BASE : ") ;
+    scanf("%f", &b);
+    printf("ENTER THE HEIGHT : ");
+    scanf("%f", &h);
+    ar = (b * h) / 2 ;
+    printf("AREA OF GIVEN TRIANGLE : %f sq units", ar);
+    return 0 ;
+}    
