a2fbb216b0cad09c3fcfa5186446966f979473b0
AJAY-10001 <127173787+Ajay-10001@users.noreply.github.com>
2023-10-06 20:16:22 +0530

    Add files via upload

---
+++ Practical 1.c
@@ -0,0 +1,15 @@
+//Let's Start Ajay
+#include <stdio.h>
+main()
+{
+    float no1, no2, sum, avg; 
+    printf("PRACTICAL 1 BY AJAY\nWAP to print sum and average of two digits");
+    printf("\nENTER 1ST NO : ");
+    scanf("%f", &no1);
+    printf("ENTER 2ND NO : ");
+    scanf("%f", &no2);
+    sum = no1 + no2 ;
+    avg = sum / 2 ;
+    printf("SUM = %f\nAVERAGE = %f", sum, avg) ;
+    return 0 ;
+}    
