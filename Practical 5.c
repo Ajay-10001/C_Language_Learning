7ea2e1de2f84023be7ed96d76a023b7dfe0bf6f3
AJAY-10001 <127173787+Ajay-10001@users.noreply.github.com>
2023-10-06 20:41:11 +0530

    Add files via upload

---
+++ Practical 5.c
@@ -0,0 +1,12 @@
+//Let's Start Ajay
+#include <stdio.h>
+main()
+{
+    float rd, ar ;
+    printf("PRACTICAL 5 BY AJAY\nWAP to calculate area of circle");
+    printf("\nVALUE OF PIE TAKEN HERE : 22/7\nENTER VALUE OF RADIUS : ") ;
+    scanf("%f", &rd);
+    ar = rd*rd*22/7 ;
+    printf("AREA OF CIRCLE : %f", ar);
+    return 0 ;
+}    
