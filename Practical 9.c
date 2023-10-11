375f3ae1e04863d27a0142c1873b80acfe7563fc
AJAY-10001 <127173787+Ajay-10001@users.noreply.github.com>
2023-10-11 08:11:18 +0530

    Add files via upload

---
+++ Practical 9.c
@@ -0,0 +1,13 @@
+//Let's Start Ajay
+#include <stdio.h>
+main()
+{
+    int no1, no2;
+    printf("PRACTICAL 9 BY AJAY\nWAP to check greatest no among two no’s.(Using ternary operator )");
+    printf("\nENTER NUMBER 1 : ") ;
+    scanf("%d", &no1);
+    printf("ENTER NUMBER 2 : ") ;
+    scanf("%d", &no2);
+    (no1>no2) ? printf("%d is greater", no1) : printf("%d is greater", no2) ;
+    return 0 ;
+}    
